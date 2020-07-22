043215f9724ca247f5bdab751bc1bb8ce98c9761
Leon006006 <50409264+Leon006006@users.noreply.github.com>
2020-07-22 15:12:33 +0200

    2-D clustering, variable cluster number

--- kMeans.py
+++ kMeans.py
@@ -12,21 +12,27 @@ class kMeans:
         self.tags = np.zeros((data.shape[1], 1))
 
     def cluster(self, numb_cluster, iterations):
+        """
+        :param numb_cluster: number of clusters to find
+        :param iterations: maximum number of iterations
+        :return: found cluster means
+        """
         # Initialize first random cluster points
         means = random.uniform(-1, 1, (2, numb_cluster, iterations))
 
-        for i in range(1, iterations - 1):
+        for i in range(1, iterations):
             # Measure distance between every data point and
             # Cluster means
             distance = np.zeros((means.shape[1], 1))
             for point in range(self.data.shape[1]):
                 for mean in range(means.shape[1]):
-                    distance[mean, 0] = linalg.norm(np.abs(np.subtract(self.data[:, point], means[:, mean, i - 1])))
+                    distance[mean, 0] = linalg.norm(np.subtract(self.data[:, point], means[:, mean, i - 1]))
                 # Assign point to closest cluster
                 self.tags[point, 0] = np.argmin(distance[:, 0])
 
+            # Loop to fit the means to assigned points
             for class_value in range(numb_cluster):
-                # get column indices for samples with this class
+                # get column indices for points with this class-tag
                 column_ix = where(self.tags == class_value)
                 # create scatter of these samples
                 plt.scatter(self.data[0, column_ix], self.data[1, column_ix])
@@ -41,12 +47,14 @@ class kMeans:
                 if magnitude_cluster == 0:
                     continue
                 means[:, class_value, i] = (1 / magnitude_cluster) * sum_cluster
+                # Plot new means
                 plt.plot(means[0, class_value, i], means[1, class_value, i], 'r*', markersize=20)
 
             plt.show()
 
-            if linalg.norm(np.subtract(means[:, :, i-1], means[:, :, i])) < 10 ** -6:
+            if linalg.norm(np.subtract(means[:, :, i-1], means[:, :, i])) < 10 ** -5:
                 print("Cluster-Means aren't changing anymore")
-                break
+                return means[:, :, i]
 
         print("Maximum iteration number reached")
+        return means[:, :, iterations-1]
--- kMeans_test.py
+++ kMeans_test.py
@@ -1,7 +1,4 @@
-import numpy as np
 import kMeans as kMeans
-import sklearn
-import matplotlib
 
 from sklearn.datasets import make_classification
 from matplotlib import pyplot as plt
@@ -10,11 +7,10 @@ from numpy import where
 X, y = make_classification(n_samples=1000, n_features=2, n_informative=2, n_redundant=0,
                            n_clusters_per_class=1, random_state=4)
 
-X[:, 0] = X[:, 0]/max(abs(X[:, 0]))
-X[:, 1] = X[:, 1]/max(abs(X[:, 1]))
 X = X.T
 plt.plot(X[0, 0], X[1, 0], 'r+')
 '''
+# Plot for base-data with "correct" classes
 # Plot loop taken from
 # https://machinelearningmastery.com/clustering-algorithms-with-python/
 for class_value in range(2):
@@ -24,6 +20,7 @@ for class_value in range(2):
     plt.scatter(X[0, column_ix], X[1, column_ix])
 plt.show()
 '''
-
+# Initialize Class with data-set
 test_means = kMeans.kMeans(X)
-test_means.cluster(4, 20)
+# Start clustering with number of clusters and iteration steps
+found_means = test_means.cluster(4, 15)
